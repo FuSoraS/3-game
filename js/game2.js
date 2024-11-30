506b82206fa95a77697362b984dcb77a1ccbf737
FuSoraS <developer.FuSoraS@gmail.com>
2024-11-30 17:19:23 -0300

    refactor: name of function js

--- js/game2.js
+++ js/game2.js
@@ -1,15 +1,15 @@
 function createRandomNum() {
-    // random number
     numAletorio = Math.floor(Math.random() * 3) + 1;
-  }
-function nombres(){
-    //Mostrar el nombre del jugador
-    const nombre = document.getElementById("jd").value;
-    document.getElementById('show').innerText= "El Jugador se llama: "+nombre;
+}
+function showName(){
+    // show of name player
+    const name = document.getElementById("input-name").value;
+    document.getElementById('show').innerText= "El Jugador se llama: "+name;
 }
 const result = document.getElementById('result');
-//Al elegir uno de los tres tener una probabilidad aletoria de poder ganar perder o empatar
-function piedra(){
+// generate a number ramdom for choose the winner
+
+function rock(){
     createRandomNum();
     if(numAletorio === 1){
         result.innerText = "Empate los dos elegieron piedra";
@@ -21,7 +21,7 @@ function piedra(){
 }
 function papel(){
     createRandomNum();
-    if(numAletorio == 1){
+    if(numAletorio === 1){
         result.innerText = "Ganaste, el contrario eligio piedra";
     }else if(numAletorio === 2){
         result.innerText = "Empate los dos elegieron papel";
